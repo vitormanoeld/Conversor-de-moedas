6fbe31e0f9c5efee336ac6b00ef6618d154ad940
Victor <vitordisccurso@gmail.com>
2025-04-21 18:02:31 -0300

    Versao de teste de divs de resultados nao muito funcional até o momento

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ const escolhaDolar = document.getElementById("doll");
 const escolhaEuro = document.getElementById("euro");
 const escolhaIene = document.getElementById("iene");
 const escolhaLibra = document.getElementById("libra");
+let moedaDolar = document.getElementById('moedaDolar');
+const pDolar = document.getElementById('victor-valor');
 
 function converterParaDolar() {
     const valorConversao = valorReal.value / 5.80
-    alert(valorConversao.toFixed(2))
+    moedaDolar.style.display = "block";
+    pDolar.textContent = valorConversao;
 }
 
 function converterParaEuro() {
     const valorConversao = valorReal.value / 6.16
-    alert(valorConversao.toFixed(2))
+    alert("€" + valorConversao.toFixed(2))
 }
 
 function converterParaIene() {
     const valorConversao = valorReal.value / 24
-    alert(valorConversao)
+    alert("¥" + valorConversao.toFixed(2))
 }
 
 function converterParaLibra() {
     const valorConversao = valorReal.value / 6.62
-    alert(valorConversao)
+    alert("£" + valorConversao.toFixed(2))
 }
